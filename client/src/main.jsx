92b92e6d135b6517768af7a835b7bf8c4c8a18d9
Hantaro171902 <chaugiahan001@gmail.com>
2025-10-13 13:17:29 +0700

    add HeroSection

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+console.log("Clerk key:", PUBLISHABLE_KEY)
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+
+   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>,
+ 
 )
