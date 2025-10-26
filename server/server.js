369448106b23a8329705a6f0a529108b28f6ff33
Hantaro171902 <chaugiahan001@gmail.com>
2025-10-26 18:18:52 +0700

    add inngest

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js'
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 const port = 3000;
@@ -13,6 +15,8 @@ await connectDB();
 app.use(express,json());
 app.use(cors());
 app.use(clerkMiddleware());
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 
 // API Routes
 app.get('/', (req, res) => res.send('Server is running!'))
